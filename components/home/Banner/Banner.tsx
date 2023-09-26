f6b9ad43b6d2851758c838e1089d209d9a92b3a3
Monir Hossain Rabby <monirhrabby.personal@gmail.com>
2023-09-26 08:32:58 +0600

    banner fixed

--- components/home/Banner/Banner.tsx
+++ components/home/Banner/Banner.tsx
@@ -1,12 +1,11 @@
 'use client'
 import React from 'react'
 import img1 from "@/images/ch-girl.png"
-import { url } from 'inspector'
-import bg from "@/images/slide-home1.jpg"
+import img2 from "@/images/w-f.png"
+import img3 from "@/images/test.png"
 import { SwiperSlide, Swiper } from 'swiper/react'
 import Image from 'next/image'
-import  {Navigation} from "swiper/modules"
-import swiperCore from "swiper"
+import  {Navigation, Autoplay} from "swiper/modules"
 
 // Import Swiper styles
 import 'swiper/css';
@@ -14,55 +13,71 @@ import 'swiper/css/navigation';
 import SwiperNavButton from './SwiperNavButton'
 import Features from './Features'
 
+import 'swiper/css/effect-fade';
+
 
 
 
 const Banner = () => {
+  const demoData = [
+    {
+      id: 1,
+      title: "From Financial Insight to Success",
+      description: "In the pursuit of success, financial focus is the anchor that keeps you grounded yet moving forward.",
+      image: img1,
+    },
+    {
+      id: 2,
+      title: "Help You To Grow Your Business",
+      description: "Turn setbacks into comebacks, and challenges into opportunities – that's the essence of business growth.",
+      image: img2
+    },
+    {
+      id: 3,
+      title: "Recover, Rebuild, Rise",
+      description: "Every step towards debt recovery is a stride towards the limitless horizon of financial independence.",
+      image: img3
+    }
+  ]
 
 
   return (
     <div className='banner' id='banner' >
-      <Swiper className='swiperS' spaceBetween={50} slidesPerView={1} 
+      <section className=''>
+      <Swiper className='swiperS pt-[200px]' spaceBetween={50} slidesPerView={1} 
       navigation={{ nextEl: "swiper-button-next", prevEl: "swiper-button-prev" }}
-      modules={[Navigation]}  
+      modules={[Navigation, Autoplay]}
+      autoplay={{
+        delay: 2500,
+        disableOnInteraction: false,
+      }}
 
         
       
       
       >
-       <SwiperSlide>
+       {demoData?.map(({id, title, description, image}) => {
+        return <SwiperSlide key={id}>
         <section className='max-w-[1400px] mx-auto flex justify-between items-center h-full'>
-        <div className='max-w-[700px]'>
+        <div className='max-w-[700px] relative'>
           <div className='tp-caption tp-resizeme font-second text-second bolder'>WE'RE NOTION RECOVERY</div>
           <div className='tp-caption tp-resizeme font-second text-second bolder text-[50px]' style={{fontSize: "50px"}}>
-            <p>Help You to Grow Your Business</p>
+            <p>{title}</p>
           </div>
-          <p className='tp-caption tp-resizeme sh3"'>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ipsam minus rem soluta dolorum ratione placeat enim, consectetur, animi facere dolorem error incidunt veniam tenetur illum totam sequi itaque laudantium molestiae.</p>
+          <p className='tp-caption tp-resizeme sh3"'>{description}</p>
         </div>
         <div className='flex-initial relative'>
-          <Image src={img1} width={800} alt='' className='relative -bottom-28'></Image>
+          <Image src={image} width={600} alt='' className='relative -bottom-[80px]'></Image>
         </div>
       </section>
         </SwiperSlide>
-        <SwiperSlide>
-        <section className='max-w-[1400px] mx-auto flex justify-between items-center h-full'>
-        <div className='max-w-[700px]'>
-          <div className='tp-caption tp-resizeme font-second text-second bolder'>WE'RE NOTION RECOVERY</div>
-          <div className='tp-caption tp-resizeme font-second text-second bolder text-[50px]' style={{fontSize: "50px"}}>
-            <p>Help You to Grow Your Business</p>
-          </div>
-          <p className='tp-caption tp-resizeme sh3"'>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ipsam minus rem soluta dolorum ratione placeat enim, consectetur, animi facere dolorem error incidunt veniam tenetur illum totam sequi itaque laudantium molestiae.</p>
-        </div>
-        <div className='flex-initial relative'>
-          <Image src={img1} width={800} alt='' className='relative -bottom-28'></Image>
-        </div>
-      </section>
-        </SwiperSlide>
+       })}
 
         <SwiperNavButton />
       </Swiper>
 
       <Features />
+      </section>
       
     </div>
   )
